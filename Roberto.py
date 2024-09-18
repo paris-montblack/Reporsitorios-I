0043c423c5e009d1d5d26b7ad2c01de9e8f6b222
Israel-MKW <robertoisrael7876@gmail.com>
2024-09-18 02:34:48 -0500

    project start

---
+++ Roberto.py
@@ -0,0 +1,12 @@
+valor1 = 20 #Trabajo de Israel Pozo
+valor2 = 5
+
+resultadoSuma=valor1 + valor2
+resultadoResta=valor1 - valor2
+resultadoMulti=valor1 * valor2
+resultadoDivi=valor1 / valor2
+
+print(f"El resultado de {valor1} + {valor2} es {resultadoSuma}")
+print(f"El resultado de {valor1} - {valor2} es {resultadoResta}")
+print(f"El resultado de {valor1} + {valor2} es {resultadoMulti}")
+print(f"El resultado de {valor1} / {valor2} es {resultadoDivi}")
